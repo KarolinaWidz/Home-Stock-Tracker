033315962a3c630c27f972f34049dd95030f5c9d
Karolina Widz <karolinawidz97@gmail.com>
2024-06-02 22:51:37 +0200

    Add hilt

--- app/src/main/java/edu/karolinawidz/homestocktracker/presentation/MainActivity.kt
+++ app/src/main/java/edu/karolinawidz/homestocktracker/presentation/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import edu.karolinawidz.homestocktracker.presentation.components.stockitem.Category
 import edu.karolinawidz.homestocktracker.presentation.components.stockitem.StockItem
 import edu.karolinawidz.homestocktracker.presentation.theme.HomeStockTrackerTheme
 import kotlinx.collections.immutable.persistentListOf
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
