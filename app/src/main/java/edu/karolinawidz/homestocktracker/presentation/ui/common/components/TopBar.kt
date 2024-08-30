464c87ca454a454493ec41e48964ba97512dd1e1
Karolina Widz <karolinawidz97@gmail.com>
2024-08-30 14:35:08 +0200

    Add sort icon changing

--- app/src/main/java/edu/karolinawidz/homestocktracker/presentation/ui/common/components/TopBar.kt
+++ app/src/main/java/edu/karolinawidz/homestocktracker/presentation/ui/common/components/TopBar.kt
@@ -14,6 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -84,7 +88,8 @@ private fun TitleText(title: String, modifier: Modifier = Modifier) {
     Text(
         modifier = modifier.fillMaxWidth(),
         text = title,
-        style = MaterialTheme.typography.titleLarge
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.onSurface,
     )
 }
 
@@ -109,10 +114,15 @@ private fun TopTitleBarActions(
     modifier: Modifier = Modifier,
     onSortClicked: () -> Unit = {}
 ) {
+    var isAsc by rememberSaveable { mutableStateOf(false) }
     Row {
-        IconButton(modifier = modifier, onClick = onSortClicked) {
+        IconButton(modifier = modifier,
+            onClick = {
+                onSortClicked()
+                isAsc = !isAsc
+            }) {
             Icon(
-                painter = painterResource(id = R.drawable.ic_sort),
+                painter = sortIcon(isAsc = isAsc),
                 contentDescription = stringResource(
                     R.string.sort_stock_items
                 ),
@@ -121,6 +131,12 @@ private fun TopTitleBarActions(
         }
     }
 }
+
+@Composable
+private fun sortIcon(isAsc: Boolean) =
+    if (isAsc) painterResource(id = R.drawable.ic_sort_asc) else painterResource(
+        id = R.drawable.ic_sort_desc
+    )
 
 @PreviewLightDark
 @Composable
